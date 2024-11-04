2f6c2d7a5992f20f609f9b34a23b9fe5eac5d650
Darya Shostak <shostak19@icloud.com>
2024-11-04 13:17:44 -0500

    done without css

--- script.js
+++ script.js
@@ -19,8 +19,38 @@ document.addEventListener("mousemove", (e) => {
   const verticalDistance = distanceFromCenter(buttonBox.y, y, buttonBox.height);
   const horizontalOffset = buttonBox.width / 2 + OFFSET;
   const verticalOffset = buttonBox.height / 2 + OFFSET;
+  if (
+    Math.abs(horizontalDistance) <= horizontalOffset &&
+    Math.abs(verticalDistance) <= verticalOffset
+  ) {
+    setButtonPosition(
+      buttonBox.x + (horizontalOffset / horizontalDistance) * 10,
+      buttonBox.y + (verticalOffset / verticalDistance) * 10
+    );
+  }
 });
 
-function distanceFromCenter(boxPosition, boxSize, mousePosition) {
+function setButtonPosition(left, top) {
+  const windowBox = document.body.getBoundingClientRect();
+  const buttonBox = evilButton.getBoundingClientRect();
+
+  if (distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+    left = windowBox.right - buttonBox.width - OFFSET;
+  }
+  if (distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+    left = windowBox.left + OFFSET;
+  }
+  if (distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+    top = windowBox.bottom - buttonBox.height - OFFSET;
+  }
+  if (distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+    top = windowBox.top + OFFSET;
+  }
+
+  evilButton.style.left = `${left}px`;
+  evilButton.style.top = `${top}px`;
+}
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
   return boxPosition - mousePosition + boxSize / 2;
 }
